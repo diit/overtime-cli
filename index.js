fee88a60353355b4637c467a11a9ec718a37868b
Gus Caplan <me@gus.host>
2017-12-02 23:06:03 -0600

    refactor

--- index.js
+++ index.js
@@ -1,61 +1,58 @@
 #!/usr/bin/env node
 
-'use strict'
-
-const Table = require('cli-table2')
-const assert = require('assert')
 const { DateTime } = require('luxon')
-const chalk = require('chalk')
-
-const vorpal = require('vorpal')()
 
 const NIGHT_TIME_START = 20
 const NIGHT_TIME_END = 9
+const GREY_BLACK = '\u001b[90m\u001b[40m'
+const BLACK_YELLOW = '\u001b[30m\u001b[43m'
 
-vorpal.command('show [regions...]', 'Generates time overlap table')
-  .alias('table')
-  .action(function (args, cb) {
-    // TODO: BETTER VALIDATION
-    assert(args.regions.length > 0)
-    const table = new Table({
-      head: args.regions
-    })
+const regions = process.argv.slice(2)
+if (regions.length === 0) throw new Error('At least one region must be specified')
 
-    // TODO: Pivot from primary
-    const getRegionsTime = region => {
-      const now = DateTime.local()
-      return now.setZone(region)
-    }
+const wrap = (a, t) => `${a} ${t} \u001b[49m\u001b[39m`
+const rows = [[], [], []]
 
-    const formatTime = time => {
-      const displayString = ` ${time.toLocaleString({
-        hour: 'numeric',
-        minute: 'numeric',
-        hour12: true
-      })} `
-      if (time.hour > NIGHT_TIME_START || time.hour < NIGHT_TIME_END) {
-        return chalk.grey.bgBlack(displayString)
-      }
-      return chalk.black.bgYellow(displayString)
-    }
+for (const region of regions) {
+  const l = Math.max(region.length + 2, 9)
+  rows[0].push(l)
+  rows[1].push(region)
+  rows[2].push(l)
+}
 
-    for (let hour = 0; hour < 24; hour++) {
-      const row = []
-      args.regions.forEach(region => {
-        row.push(formatTime(getRegionsTime(region).plus({
-          hours: hour
-        }).set({
-          minutes: 0
-        })))
-      })
-      table.push(row)
-    }
+const spacerRow = `├${rows[0].map((l) => '─'.repeat(l)).join('┼')}┤`
 
-    this.log(table.toString())
-    cb(null, table.toString())
-  })
+let first = true
+for (let hour = 0; hour < 24; hour++) {
+  if (first) {
+    first = false
+  } else {
+    rows.push(spacerRow)
+  }
 
-vorpal
-  .delimiter('$ ')
-  .show()
-  .parse(process.argv)
+  const row = []
+
+  for (const i in regions) {
+    const region = regions[i]
+    const t = DateTime.local()
+      .setZone(region)
+      .plus({ hours: hour })
+      .set({ minutes: 0 })
+
+    const displayString = t.toLocaleString({ hour: 'numeric', minute: 'numeric' })
+    const color = t.hour > NIGHT_TIME_START || t.hour < NIGHT_TIME_END ? GREY_BLACK : BLACK_YELLOW
+    const wrapped = wrap(color, displayString)
+    const l = rows[0][i] - displayString.length
+    row.push(` ${wrapped}${' '.repeat(l - 3)}`)
+  }
+
+  rows.push(`│${row.join('│')}│`)
+}
+
+rows.push(`└${rows[0].map((l) => '─'.repeat(l)).join('┴')}┘`)
+
+rows[0] = `┌${rows[0].map((l) => '─'.repeat(l)).join('┬')}┐`
+rows[1] = `│ ${rows[1].map((r) => `\u001b[31m${r.padEnd(7, ' ')}\u001b[39m`).join(' │ ')} │`
+rows[2] = `├${rows[2].map((l) => '─'.repeat(l)).join('┼')}┤`
+
+process.stdout.write(`${rows.join('\n')}\n`)
